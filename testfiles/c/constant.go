948a6d26e1d7b3b162ed5df24be5d9ab5cf5036a
54mch4n <54mch4n.1111@gmail.com>
2020-05-09 00:43:22 +0900

    add: constant.go

---
+++ testfiles/c/constant.go
@@ -0,0 +1,38 @@
+// THIS FILE IS A GENERATED CODE. DO NOT EDIT
+package task
+
+import "strconv"
+
+type BoolCriteria string
+
+const (
+	BoolCriteriaEmpty BoolCriteria = ""
+	BoolCriteriaTrue  BoolCriteria = "true"
+	BoolCriteriaFalse BoolCriteria = "false"
+)
+
+func (src BoolCriteria) Bool() bool {
+	return src == BoolCriteriaTrue
+}
+
+type IntegerCriteria string
+
+const (
+	IntegerCriteriaEmpty IntegerCriteria = ""
+)
+
+func (str IntegerCriteria) Int() int {
+	i32, err := strconv.Atoi(string(str))
+	if err != nil {
+		return -1
+	}
+	return i32
+}
+
+func (str IntegerCriteria) Int64() int64 {
+	i64, err := strconv.ParseInt(string(str), 10, 64)
+	if err != nil {
+		return -1
+	}
+	return i64
+}
