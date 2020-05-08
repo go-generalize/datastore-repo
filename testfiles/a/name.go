cca83db14427b89ac5fa810ff0cc473a4aaba561
54mch4n <54mch4n.1111@gmail.com>
2020-05-08 19:53:24 +0900

    fix: minor correction #4

--- testfiles/a/name.go
+++ testfiles/a/name.go
@@ -7,10 +7,13 @@ import (
 //go:generate repo_generator Name github.com/go-generalize/repo_generator/testfiles/a
 //go:generate gofmt -w ./
 
+// Name 拡張インデックスあり
 type Name struct {
 	ID      int64     `datastore:"-" datastore_key:""` // supported type: string, int64, *datastore.Key
 	Created time.Time `datastore:"created"`
-	Desc    string    `datastore:"description"`
+	Desc    string    `datastore:"description" filter:"l"`  // supported word: m/matching(Default), l/like, p/prefix, TODO s/suffix
+	Desc2   string    `datastore:"description2" filter:"p"` // supported word: m/matching(Default), l/like, p/prefix, TODO s/suffix
 	Done    bool      `datastore:"done"`
 	Count   int       `datastore:"count"`
+	Indexes []string  `datastore:"indexes"`
 }
--- testfiles/a/task.go
+++ testfiles/a/task.go
@@ -7,13 +7,13 @@ import (
 //go:generate repo_generator Task github.com/go-generalize/repo_generator/testfiles/a
 //go:generate gofmt -w ./
 
+// Task 拡張インデックスなし
 type Task struct {
-	ID      int64     `datastore:"-" datastore_key:""`     // supported type: string, int64, *datastore.Key
-	Desc    string    `datastore:"description" filter:"l"` // supported word: m/matching(Default), l/like, p/prefix, TODO s/suffix
+	ID      int64     `datastore:"-" datastore_key:""` // supported type: string, int64, *datastore.Key
+	Desc    string    `datastore:"description"`
 	Created time.Time `datastore:"created"`
 	Done    bool      `datastore:"done"`
 	Done2   bool      `datastore:"done2"`
 	Count   int       `datastore:"count"`
 	Count64 int64     `datastore:"count64"`
-	Indexes []string  `datastore:"indexes"` // for XIAN
 }
